a9bff3b205922dbe6e5a9fbda1ea98fa12c76ebe
HariSivaraj <harisivaraj@hotmail.com>
2024-01-24 20:28:51 +0530

    collection class added

---
+++ src/stringOps/collectionCode.java
@@ -0,0 +1,12 @@
+package stringOps;
+
+public class collectionCode {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("This class mainly written for collection framework");
+
+	}
+
+}
